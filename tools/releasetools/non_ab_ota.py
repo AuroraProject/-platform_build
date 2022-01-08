hlcynprjct-staging/build_make

releasetools: Add our ASCII logo for recovery Squashed of: 1. include our dope ASCII logo for twrp
Change-Id: I4d69ce91e77ece2730d1e3d72308611ad07c8a80
Signed-off-by: Amitava Mitra <mitraamitava7@gmail.com>

2.
tools: releasetools: improvise our ASCII logo

Change-Id: I32e83e0010210f1c957c96158731512ab060e21c
Signed-off-by: Amitava Mitra <mitraamitava7@gmail.com>

[kubersharma001]:
* Adapt to android-12.0
Ref: https://android.googlesource.com/platform/build/+/cff4d7606dd195d8c2726867024b310a23a4ac8b

Change-Id: I4d69ce91e77ece2730d1e3d72308611ad07c8a80
Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

tools/releasetools/non_ab_ota.py
@@ -211,6 +211,23 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  android_version = target_info.GetBuildProp("ro.build.version.release")
+  build_id = target_info.GetBuildProp("ro.build.id")
+  build_date = target_info.GetBuildProp("ro.build.date")
+  security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.device")
+
+  script.Print("=================================================");
+  script.Print("");
+  script.Print("              Welcome to hlcynprjct!             ");
+  script.Print("");
+  script.Print("=================================================");
+  script.Print(" Android version  : %s"%(android_version));
+  script.Print(" Build id         : %s"%(build_id));
+  script.Print(" Build date       : %s"%(build_date));
+  script.Print(" Security patch   : %s"%(security_patch));
+  script.Print(" Device           : %s"%(device));
+  script.Print("=================================================");
   device_specific.FullOTA_InstallBegin()
 
   # All other partitions as well as the data wipe use 10% of the progress, and

AuroraProject/-platform_build

tools/releasetools/non_ab_ota.py
@@ -211,6 +211,23 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  android_version = target_info.GetBuildProp("ro.build.version.release")
+  build_id = target_info.GetBuildProp("ro.build.id")
+  build_date = target_info.GetBuildProp("ro.build.date")
+  security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.device")
+
+  script.Print("=================================================");
+  script.Print("");
+  script.Print("                 Welcome to Aurora!              ");
+  script.Print("");
+  script.Print("=================================================");
+  script.Print(" Android version  : %s"%(android_version));
+  script.Print(" Build id         : %s"%(build_id));
+  script.Print(" Build date       : %s"%(build_date));
+  script.Print(" Security patch   : %s"%(security_patch));
+  script.Print(" Device           : %s"%(device));
+  script.Print("=================================================");
   device_specific.FullOTA_InstallBegin()
 
   # All other partitions as well as the data wipe use 10% of the progress, and